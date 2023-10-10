df0d906d0dc1bce0d978b3c451205a530381d6d5
ElenaTsai <140524058+ElenaTsai@users.noreply.github.com>
2023-10-10 13:33:13 +0000

    1010

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(f'你剛說:{even.message.text}')
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
